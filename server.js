34d4fd787b0028739894c24eea45d70ea47f70cd
yusluv1234 <yusluv124@gamil.com>
2020-12-31 23:14:22 +0100

    adding route, users, contacts and auth

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API...' }))
+
+// app.get('/', (req, res) => res.send('Hello World'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
